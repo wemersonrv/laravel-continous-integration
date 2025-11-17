195b8ba8e1f68ed154ed37fcd50683b0babd5a03
Wemerson Guimaraes <wemersonrv@gmail.com>
2025-11-17 16:41:51 -0300

    wip

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -19,6 +19,7 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
+        // This is a ranrom comment to demonstrate code changes.
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
