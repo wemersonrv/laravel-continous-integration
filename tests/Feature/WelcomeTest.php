f9140c4c08b56c811387b69637114e04282f836d
Wemerson Guimaraes <wemersonrv@gmail.com>
2025-11-18 19:01:56 -0300

    Adicionando 4 repetições no teste para ver se passa

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -2,9 +2,18 @@
 
 use App\Models\User;
 
-// testing redis
+it('increments the page count for each visit', function () {
+    Redis::del('landing-page-views');
+
+    $this->get('/');
+    $this->get('/');
+    $this->get('/');
+
+    expect(Redis::get('landing-page-views'))->toEqual(3);
+});
+
 it('provides users in random paginated order', function () {
-    $users = User::factory(4)->create();
+    $users = User::factory(5)->create();
 
     $users = collect($this->get('/')->viewData('users')->items())
         ->merge($this->get('/?page=2')->viewData('users')->items());
