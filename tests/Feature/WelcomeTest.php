73c03fe3bce84c402821f4cc5613e6475b027856
Wemerson Guimaraes <wemersonrv@gmail.com>
2025-11-18 20:09:44 -0300

    Wip

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -16,7 +16,6 @@ it('increments the page count for each visit', function () {
 it('provides users in random paginated order', function () {
     $users = User::factory(4)->create();
 
-
     $users = collect($this->get('/')->viewData('users')->items())
         ->merge($this->get('/?page=2')->viewData('users')->items());
 
